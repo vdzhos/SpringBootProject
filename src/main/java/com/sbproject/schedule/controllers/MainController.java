f978010fcd30458d2089d9302f8f0b360288865f
Illia Kuznets <illiakuznets@gmail.com>
2021-09-29 22:47:58 +0300

    added visualisation of Teacher on html page

--- src/main/java/com/sbproject/schedule/controllers/MainController.java
+++ src/main/java/com/sbproject/schedule/controllers/MainController.java
@@ -3,6 +3,7 @@ package com.sbproject.schedule.controllers;
 import com.sbproject.schedule.services.implementations.SpecialtyServiceImpl;
 import com.sbproject.schedule.services.implementations.SubjectServiceImpl;
 import com.sbproject.schedule.services.interfaces.LessonService;
+import com.sbproject.schedule.services.interfaces.TeacherService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -21,13 +22,15 @@ public class MainController {
     private SubjectServiceImpl subjectService;
     @Autowired
     private LessonService lessonService;
-
+    @Autowired
+    private TeacherService teacherService;
 
     @GetMapping
     public String getAll(Model model){
         model.addAttribute("specialties",specialtyService.getAll());
         model.addAttribute("subjects",subjectService.getAll());
         model.addAttribute("lessons", lessonService.getAll());
+        model.addAttribute("teachers", teacherService.getAll());
         return "main";
     }
 
--- src/main/java/com/sbproject/schedule/controllers/TeacherController.java
+++ src/main/java/com/sbproject/schedule/controllers/TeacherController.java
@@ -1,6 +1,9 @@
 package com.sbproject.schedule.controllers;
 
 import com.sbproject.schedule.services.implementations.TeacherServiceImpl;
+import com.sbproject.schedule.services.interfaces.LessonService;
+import com.sbproject.schedule.services.interfaces.TeacherService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -11,25 +14,30 @@ import org.springframework.web.bind.annotation.RequestParam;
 @RequestMapping("/teacher")
 public class TeacherController {
 
-    private TeacherServiceImpl teacherServiceImpl;
+    private TeacherService teacherService;
+
+    @Autowired
+    public TeacherController(TeacherService teacherService) {
+        this.teacherService = teacherService;
+    }
 
     @PostMapping("/add")
     public String addTeacher(@RequestParam String name, Model model){
-        teacherServiceImpl.addTeacher(name);
+        teacherService.addTeacher(name);
         //put info about success/failure into the model
         return "redirect:/";
     }
 
     @PostMapping("/delete")
     public String deleteTeacher(@RequestParam Long id, Model model){
-        teacherServiceImpl.deleteTeacher(id);
+        teacherService.deleteTeacher(id);
         //put info about success/failure into the model
         return "redirect:/";
     }
 
     @PostMapping("/update")
-    public String updateTeacher(@RequestParam Long id, @RequestParam String newName, Model model){
-        teacherServiceImpl.updateTeacher(id, newName);
+    public String updateTeacher(/*@RequestParam Long id, @RequestParam String newName, */Model model){
+        //teacherService.updateTeacher(id, newName);
         //put info about success/failure into the model
         return "redirect:/";
     }
