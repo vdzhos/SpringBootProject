3046f5f313a1820228b815e7049682eb605c6f27
Sitkov Illia <illiasitkov@gmail.com>
2021-11-04 08:29:18 +0200

    SpecialtyRestController extended

---
+++ src/main/java/com/sbproject/schedule/exceptions/subject/SubjectNotFoundException.java
@@ -0,0 +1,8 @@
+package com.sbproject.schedule.exceptions.subject;
+
+public class SubjectNotFoundException extends RuntimeException {
+
+    public SubjectNotFoundException(String message) {
+        super(message);
+    }
+}
