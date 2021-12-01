e440da013d977cbd4e76635e929bf62f510e1d75
--replace-all <dgosan37@gmail.com>
2021-12-01 20:03:16 +0200

    Added DataJPATests and SecurityTests for Lesson.

--- src/main/java/com/sbproject/schedule/services/interfaces/LessonService.java
+++ src/main/java/com/sbproject/schedule/services/interfaces/LessonService.java
@@ -9,9 +9,9 @@ public interface LessonService {
 
     Lesson getLessonById(Long id) throws NoLessonWithSuchIdFound;
     Iterable<Lesson> getAll();
-    boolean updateLesson(Long id, Lesson.Time value, Long subjId, Long teachId, SubjectType subjectType, String weeks, Room r, DayOfWeek of);
+    Lesson updateLesson(Long id, Lesson.Time value, Long subjId, Long teachId, SubjectType subjectType, String weeks, Room r, DayOfWeek of);
     Lesson updateLesson(Lesson lesson) throws NoLessonWithSuchIdFound;
-    boolean addLesson(Lesson.Time value, Long subjId, Long teachId, SubjectType subjectType, String weeks, Room r, DayOfWeek of);
+    Lesson addLesson(Lesson.Time value, Long subjId, Long teachId, SubjectType subjectType, String weeks, Room r, DayOfWeek of);
     Lesson addLesson(Lesson lesson);
     boolean lessonExistsById(Long id);
     void deleteLesson(Long id) throws NoLessonWithSuchIdFound;
---
+++ src/test/java/com/sbproject/schedule/lesson/LessonSecurityTest.java
@@ -0,0 +1,73 @@
+package com.sbproject.schedule.lesson;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.jupiter.api.Assertions;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.security.test.context.support.WithMockUser;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MvcResult;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.context.WebApplicationContext;
+
+import static org.springframework.security.test.web.servlet.setup.SecurityMockMvcConfigurers.springSecurity;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+@AutoConfigureMockMvc
+public class LessonSecurityTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Autowired
+    private WebApplicationContext context;
+
+    @Before
+    public void setup() {
+        mockMvc = MockMvcBuilders.webAppContextSetup(context).apply(springSecurity()).build();
+        System.out.println(mockMvc);
+    }
+
+    @WithMockUser(username = "usr", password = "1111", roles = {"ADMIN","REGULAR"})
+    @Test
+    public void givenLessonsURI_whenRoleAdmin_thenResponseOK() throws Exception {
+        MvcResult result = mockMvc.perform(get("/REST/lessons/all")).andDo(print())
+                .andExpect(status().isOk()).andReturn();
+
+        Assertions.assertEquals(200, result.getResponse().getStatus());
+    }
+
+    @WithMockUser(username = "usr", password = "1111", roles = "ANONYMOUS")
+    @Test
+    public void givenLessonsURI_whenRoleAnonymous_thenResponseForbidden() throws Exception {
+        MvcResult result = mockMvc.perform(get("/REST/lessons/all")).andDo(print())
+                .andExpect(status().is(403)).andReturn();
+
+        Assertions.assertEquals(403, result.getResponse().getStatus());
+    }
+
+    @WithMockUser(username = "usr", password = "1111", roles = {"REGULAR", "ADMIN"})
+    @Test
+    public void givenLessonsURI_whenRoleAdminOrRegular_getLesson_thenResponseOK() throws Exception {
+        MvcResult result = mockMvc.perform(get("/REST/lessons/1")).andDo(print())
+                .andExpect(status().isOk()).andReturn();
+        Assertions.assertEquals(200, result.getResponse().getStatus());
+    }
+
+    @WithMockUser(username = "usr", password = "1111", roles = "ANONYMOUS")
+    @Test
+    public void givenLessonsURI_whenRoleAnonymous_getLesson_thenResponseForbidden() throws Exception {
+        MvcResult result = mockMvc.perform(get("/REST/lessons/1")).andDo(print())
+                .andExpect(status().isForbidden()).andReturn();
+        Assertions.assertEquals(403, result.getResponse().getStatus());
+    }
+
+}
