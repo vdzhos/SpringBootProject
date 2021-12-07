18adb6e3a63e5e6543705d983a1cc69ff270f20e
Hlib Kalinichev <hlib.kalinichev@ukma.edu.ua>
2021-12-07 22:03:27 +0200

    Merge Commit

--- src/main/java/com/sbproject/schedule/controllers/ScheduleTableController.java
+++ src/main/java/com/sbproject/schedule/controllers/ScheduleTableController.java
@@ -15,7 +15,6 @@ import com.sbproject.schedule.models.Schedule;
 import com.sbproject.schedule.xlsx.ScheduleDownloader;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
